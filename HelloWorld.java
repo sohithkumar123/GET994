41020850efd0feb12d8cd9e3ab609799c259c1d6
sohith kumar <sohithkumar87@gmail.com>
2022-03-01 12:45:57 +0530

    2nd commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
-public static void main(String [] args){
-System.out.println("rey sunny nuv super ra");
+  
+   public static void main(String [] args){
+       System.out.println("rey sunny nuv super ra");
+int a=10;
 }
 }
